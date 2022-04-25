96250a365c178fed3e53144e57cbef5de22eb9a6
Surat Mukker <surat@immersa.co>
2022-04-25 16:06:03 -0700

    change the quotes around so that json can parse the string

--- duplo-service-status/tests/test_duplo_services_status.sh
+++ duplo-service-status/tests/test_duplo_services_status.sh
@@ -2,6 +2,6 @@ export INPUT_HOST="https://immersa-dev.duplocloud.net/"
 export INPUT_TENANT="dev01"
 export INPUT_TENANT_ID="ed684c6c-9876-4fde-999a-849d6586a4a7"
 export INPUT_TOKEN=$duplo_dev_token
-export INPUT_SERVICES='["all"]'
+export INPUT_SERVICES='["api", "btcs", "ftcs"]'
 
 python3 ../duplo_services_status.py
