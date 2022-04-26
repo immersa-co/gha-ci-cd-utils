07572996eac58c71a367048e3b7bb6a6a9533e1b
Surat Mukker <surat@immersa.co>
2022-04-25 19:15:37 -0700

    Add github action to fetch duplo service details

---
+++ duplo-service-details/duplo_services_details.py
@@ -0,0 +1,60 @@
+import requests
+import json
+import os
+import time
+
+# TODO: Future enhancement create an action to look up all tenants and Ids using
+#   https://immersa-dev.duplocloud.net/admin/GetTenantsForUser
+#   response is an array of maps with 2 keys of interest
+#   [{..."TenantId":"<id>","AccountName":"<name>","...}, ...]
+
+
+def fetch_duplo_service_details(host, tenant, tenant_id, token, services_array):
+    # Send GET request to Duplo API to get service information
+    duplo_url = f'{host}/subscriptions/{tenant_id}/GetPods'
+    duplo_headers = {'Authorization': f"Bearer {token}"}
+
+    response = requests.get(duplo_url, headers=duplo_headers)
+
+    if not response.ok:
+        print(f'Trouble Getting services for {tenant} from duplo cloud {duplo_url} ')
+        print(response.content)
+        raise Exception(response.json())
+    else:
+        # process and get the service list
+        duplo_response = json.loads(response.content.decode())
+        # Initializing dictionary to hold scraped data
+        service_details = dict()
+        include_all = ('all' in services_array)
+        # Loop through response contents and fill out data for running services
+        # If the Duplo service name contains 'duploinfrasvc', then ignore it when adding data to service_details
+        for service in duplo_response:
+            service_name = service["Name"]
+            if (include_all and not service_name.endswith('duploinfrasvc')) or (service_name in services_array):
+                ecr_repo, i, image_tag = service["Containers"][0]["Image"].rpartition(':')
+                service_details[service_name] = image_tag
+    return service_details
+
+
+def run_action() -> None:
+    # host, tenant, tenant_id, token, services
+    host = os.environ["INPUT_HOST"]
+    tenant = os.environ["INPUT_TENANT"]
+    tenant_id = os.environ["INPUT_TENANT_ID"]
+    token = os.environ["INPUT_TOKEN"]
+    services = os.environ["INPUT_SERVICES"]
+
+    try:
+        if services != '':
+            services_array = json.loads(services)
+        else:
+            services_array = ['all']
+        service_details = fetch_duplo_service_details(host, tenant, tenant_id, token, services_array)
+        print(f"::set-output name=service_details::{json.dumps(service_details)}{os.linesep}")
+    except Exception as e:
+        print(f"::error ::{str(e)}{os.linesep}")
+        raise e
+
+
+if __name__ == "__main__":
+    run_action()
---
+++ duplo-service-details/tests/test_duplo_services_status_docker_details.sh
@@ -0,0 +1,10 @@
+export INPUT_HOST="https://immersa-dev.duplocloud.net/"
+export INPUT_TENANT="dev01"
+export INPUT_TENANT_ID="ed684c6c-9876-4fde-999a-849d6586a4a7"
+export INPUT_TOKEN=$duplo_dev_token
+export INPUT_SERVICES='["all"]'
+
+image_tag='test:test-gha-duplo-services-details-v1'
+docker build --platform linux/amd64 -t $image_tag ..
+docker run -e INPUT_HOST -e INPUT_TENANT -e INPUT_TENANT_ID \
+-e INPUT_TOKEN -e INPUT_SERVICES -it --platform linux/amd64 $image_tag
