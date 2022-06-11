9248564262a3fd17ecf5f44d7a64dcfc62d685eb
Surat Mukker <surat@immersa.co>
2022-06-11 16:34:43 -0700

    Remove redundant test file and fix the filename in action.yaml

--- yaml_config_as_json/tests/test_config_reader.sh
+++
@@ -1,8 +0,0 @@
-export SERVICEREPO=immersa-co/api
-export CONFIGREF=main
-export TENANTNAME=dev01
-export GHATOKEN="$(python3 ~/utils/generateGithubAppToken.py)"
-
-#python3 ../main.py
-
-../config_reader.sh
