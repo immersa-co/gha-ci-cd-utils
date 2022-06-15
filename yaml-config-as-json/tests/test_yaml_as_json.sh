25fdc8d4480733d7a6c172d89ba1f5ffc30f495c
Surat Mukker <surat@immersa.co>
2022-06-15 14:00:46 -0700

    Add support for reading multiple config files in one go

--- yaml-config-as-json/tests/test_yaml_as_json.sh
+++ yaml-config-as-json/tests/test_yaml_as_json.sh
@@ -2,16 +2,25 @@ export github_app_key=$github_app_key_ro
 export github_app_id=$github_app_id_ro
 export github_install_id=$github_install_id_ro
 
-export GITREPO="immersa-co/api"
-export GITREF="add-ci-configs"
-export FILEPATH="configs"
-export FILENAME="dev01"
 export FILEEXT=".yaml"
 export GHATOKEN="$(python3 ~/utils/generateGithubAppToken.py)"
 
+export GITREPO_REF_FILEPATH="immersa-co/api/main/configs"
+export GITREPOS_REF_FILEPATH="none"
+
+export FILENAME="dev01"
 ../yaml_as_json.sh
 
+export GITREPO_REF_FILEPATH="immersa-co/api/add-ci-configs/configs"
 export FILENAME="ci01"
-export GHATOKEN="$(python3 ~/utils/generateGithubAppToken.py)"
+../yaml_as_json.sh
 
+export GITREPO_REF_FILEPATH="none"
+export GITREPOS_REF_FILEPATH='{"api":"immersa-co/api/main/configs", "btcs":"immersa-co/btcs/main/configs"}'
+
+export FILENAME="dev01"
+../yaml_as_json.sh
+
+export GITREPOS_REF_FILEPATH='{"api":"immersa-co/api/add-ci-configs/configs", "btcs":"immersa-co/btcs/add-ci-configs/configs"}'
+export FILENAME="ci01"
 ../yaml_as_json.sh
--- yaml-config-as-json/yaml_as_json.sh
+++ yaml-config-as-json/yaml_as_json.sh
@@ -1,21 +1,48 @@
 #!/bin/bash
 
+export REPLACE_STR="ci0x"
+fetch_multiple_yaml_as_json() {
+  jsonKvs=''
+  export kvs=$(echo $GITREPOS_REF_FILEPATH | jq 'to_entries[] | "\(.key):\(.value)"'  | tr "\n" " ")
+  eval kvs_arr="($kvs)"
+  for kv in "${kvs_arr[@]}"
+  do
+    key="${kv%:*}"
+    repo_ref_filepath="${kv#*:}"
+    jsonStr=$(fetch_yaml_as_json $repo_ref_filepath)
+    jsonKvs+="\""$key"\":"$jsonStr","
+  done
+#  printf -v jsonArr "%s," "${jsonKvs[@]}"
+  echo "{${jsonKvs%,}}"
+#   echo "${jsonKvs[@]}"
+}
 
 fetch_yaml_as_json() {
+  if [[ -z $1 ]]
+  then
+    repo_ref_filepath=$GITREPO_REF_FILEPATH
+  else
+    repo_ref_filepath=$1
+  fi
 
   if [[ $FILENAME == ci* ]]
   then
-    export readfile="ci0x"
+    export readfile=$REPLACE_STR
   else
     export readfile=$FILENAME
   fi
-  yamlcontent=$(curl --header "Authorization: token $GHATOKEN" https://raw.githubusercontent.com/$GITREPO/$GITREF/$FILEPATH/$readfile$FILEEXT)
+  yamlcontent=$(curl --header "Authorization: token $GHATOKEN" https://raw.githubusercontent.com/$repo_ref_filepath/$readfile$FILEEXT)
   jsoncontent=$(echo "$yamlcontent" | sed '/#.*/d' | yq -o=json)
   if [[ $FILENAME == ci* ]]
   then
-    jsoncontent=${jsoncontent//ci0x/$FILENAME} 
+    jsoncontent=${jsoncontent//$REPLACE_STR/$FILENAME}
   fi
   printf "%s" "${jsoncontent}"
 }
 
-fetch_yaml_as_json
+if [[ $GITREPO_REF_FILEPATH == "none" ]]
+then
+  fetch_multiple_yaml_as_json
+else
+  fetch_yaml_as_json "$GITREPO_REF_FILEPATH"
+fi
