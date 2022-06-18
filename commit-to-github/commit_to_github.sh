debf8fb94c0f34d15c38175a3def9a9843e2e0cb
Surat Mukker <surat@immersa.co>
2022-06-18 13:47:45 -0700

    strip \n after encode

--- commit-to-github/commit_to_github.sh
+++ commit-to-github/commit_to_github.sh
@@ -34,7 +34,7 @@ process_commit_steps() {
     echo $FILE_UPDATED
     echo $COMMIT_SHA
   else
-    fileContentBase64=$(echo "$FILECONTENTS" | base64)
+    fileContentBase64=$(echo "$FILECONTENTS" | base64 | sed 's/\\n//g')
     echo $fileContentBase64
     echo "{\"message\": \"$MESSAGE\", \"content\": \"$fileContentBase64\", \"sha\": \"$currentSha\", \"branch\" : \"$GITBRANCH\"}"
 
