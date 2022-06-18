096e95ef835fbbc26051536b9915f9f98bd54f27
Surat Mukker <surat@immersa.co>
2022-06-18 14:41:20 -0700

    Merging after getting it working on a temp branch

--- commit-to-github/commit_to_github.sh
+++ commit-to-github/commit_to_github.sh
@@ -14,26 +14,22 @@ process_commit_steps() {
    # does not work, sed does. Using "Accept: application/vnd.github.VERSION.raw" will give un-encoded contents
   gitContents=$(curl -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $GHATOKEN" \
                  https://api.github.com/repos/$fileUrl?ref=$GITBRANCH)
-  currentContent=$(echo $gitContents | jq '.content' | tr -d '"' | sed 's/\\n//g')
+  currentContentEncoded=$(echo $gitContents | jq '.content' | tr -d '"' | sed 's/\\n//g')
   currentSha=$(echo $gitContents | jq '.sha' | tr -d '"')
   echo "$gitContents"
-  echo $currentContent
-  echo $currentSha
+  # Base 64 encode the desired contents and compare with what is in repo. If same then return the commit-sha of
+  # head otherwise commit and return the sha. the only way to make encode/decode work reliably across testing locally
+  # on MAC and the actual github action is to use the -w 0 flag on Linux and not use on MAC
+  fileContentBase64=$(echo "$FILECONTENTS" | base64 $BASE64OPTIONS)
 
-  # BAse 64 encode the desired contents and compare with what is in repo. If same then return the commit-sha of
-  # head otherwise commit and return the sha
-  fileContentBase64=$(echo "$FILECONTENTS" | base64)
-
-  if [[ "$currentContent" == "$fileContentBase64" ]]
+  if [[ "$currentContentEncoded" == "$fileContentBase64" ]]
   then
     # Get the commit sha of the tree
     repoRefResponse=$(curl -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $GHATOKEN" \
         https://api.github.com/repos/$GITREPO/git/ref/heads/$GITBRANCH)
     COMMIT_SHA=$(echo $repoRefResponse | jq '.object.sha' | tr -d '"')
     FILE_UPDATED='False'
-    echo $repoRefResponse
-    echo $FILE_UPDATED
-    echo $COMMIT_SHA
+    echo "$repoRefResponse"
   else
     # Make a commit to the branch
     response=$(curl -X PUT -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $GHATOKEN" \
@@ -41,9 +37,7 @@ process_commit_steps() {
        https://api.github.com/repos/$fileUrl)
     COMMIT_SHA=$(echo $response | jq '.commit.sha' | tr -d '"')
     FILE_UPDATED='True'
-    echo $response
-    echo $FILE_UPDATED
-    echo $COMMIT_SHA
+    echo "$response"
   fi
 
   echo "::set-output name=commitSha::$(echo $COMMIT_SHA)"
